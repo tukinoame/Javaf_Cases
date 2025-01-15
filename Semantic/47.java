e1fa7b685d8cd6fabcbd99b35522d0223f3415ae
tukinoame <yuebao457896@gmail.com>
2025-01-15 12:20:10 +0800

    Update 47.java

--- Semantic/47.java
+++ Semantic/47.java
@@ -6,7 +6,7 @@ interface MyInterface {
 
 class Test {
     public static void main(String[] args) {
-        MyInterface myInterface = new MyInterface(42) { 
+        MyInterface myInterface = new <Object>MyInterface(42) { 
             @Override
             public void doSomething() {
                 System.out.println("Doing something");
